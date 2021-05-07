a5f643265f06d26c4ba380fff903a5e069d95818
Hubert <40564309+Qzername@users.noreply.github.com>
2021-05-07 14:15:41 +0200

    Download Demo
    Created demo for future updates

---
+++ Forester/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace Forester
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Download_Click(object sender, RoutedEventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFileAsync(
+                    new Uri("http://localhost:5000/api/Download"),
+                    "./test.zip"
+                );
+            }
+        }
+    }
+}
---
+++ ForesterAPI/Controllers/Download.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForesterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Download : ControllerBase
+    {
+        // GET: api/<Download>
+        [HttpGet]
+        public FileContentResult DownloadDocument()
+        {
+            string filePath = "./test.zip";
+            string fileName = "test.zip";
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileBytes, "application/force-download", fileName);
+        }
+    }
+}
