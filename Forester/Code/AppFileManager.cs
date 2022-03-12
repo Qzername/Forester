7f0e9b9e2c05cb91fcd94c7620e75f4e786f7b30
Hubert <40564309+Qzername@users.noreply.github.com>
2022-03-12 17:50:00 +0100

    Upload new version section

--- Forester/Code/AppFileManager.cs
+++ Forester/Code/AppFileManager.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Forester.Code
+namespace Forester
 {
     public static class AppFileManager
     {
--- Forester/ViewModels/App/Developer/ManageAppViewModel.cs
+++ Forester/ViewModels/App/Developer/ManageAppViewModel.cs
@@ -2,18 +2,23 @@
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
+using Forester;
 using Forester.Models.API;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Forester.ViewModels.App.Developer
 {
     public class ManageAppViewModel : ViewModelBase
     {
+        #region Basic App Information
         public string _appName, _quickDescription, _description, _currentVersion;
 
         public string appName
@@ -74,14 +79,50 @@ namespace Forester.ViewModels.App.Developer
             set => this.RaiseAndSetIfChanged(ref _errorSwitchChanges, value);
         }
 
+        #endregion
+
+        #region Upload New Version
+        string _newVersion;
+        public string newVersion
+        {
+            get => _newVersion;
+            set => this.RaiseAndSetIfChanged(ref _newVersion, value);
+        }
+
+        string _newVersionCurrentPath;
+        public string newVersionCurrentPath
+        {
+            get => _newVersionCurrentPath;
+            set => this.RaiseAndSetIfChanged(ref _newVersionCurrentPath, value);
+        }
+
+        float _versionPBValue;
+        public float versionPBValue
+        {
+            get => _versionPBValue;
+            set => this.RaiseAndSetIfChanged(ref _versionPBValue, value);
+        }
+
+        string _errorNewVersion;
+        public string errorNewVersion
+        {
+            get => _errorNewVersion;
+            set => this.RaiseAndSetIfChanged(ref _errorNewVersion, value);
+        }
+
+        string pathToFile;
+        #endregion
+
         Application currentApp;
         DeveloperViewModel developerVM;
 
         public ManageAppViewModel(DeveloperViewModel developerVM)
         {
+            newVersionCurrentPath = "Current path: None";
             this.developerVM = developerVM;
         }
 
+        #region Basic app information 
         public void SwitchChanges()
         {
             if (makeChanges)
@@ -216,7 +257,9 @@ namespace Forester.ViewModels.App.Developer
 
             currentVersion = "Current version: " + app.version;
         }
+        #endregion
 
+        #region Upload New Version
         public void SetApp(Application app)
         {
             switchChangesButtonContent = "Make changes";
@@ -225,5 +268,89 @@ namespace Forester.ViewModels.App.Developer
 
             SetBasicInformation(app);
         }
+
+        public async void SelectPath()
+        {
+            var dialog = new OpenFolderDialog();
+
+            if (Avalonia.Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                string? result = await dialog.ShowAsync(desktop.MainWindow);
+
+                if (result is null)
+                    return;
+
+                 pathToFile = result;
+                 newVersionCurrentPath = "Current path: " + pathToFile;
+            }
+        }
+
+        public void SubmitNewVersion()
+        {
+            errorNewVersion = "";
+            versionPBValue = 0f;
+
+            if (pathToFile == string.Empty)
+            {
+                errorNewVersion = "Path cannot be empty";
+                return;
+            }
+
+            Thread uploadThread = new Thread(UploadThread);
+            uploadThread.Start();
+        }
+
+        public void UploadThread()
+        {
+            string path = AppFileManager.PrepareApp(pathToFile);
+
+            WebClient client = new WebClient();
+            client.UploadProgressChanged += Client_UploadProgressChanged;
+            client.UploadFileCompleted += Client_UploadFileCompleted;
+
+            errorNewVersion = "Uploading...";
+
+            ServerConnection.Upload(ref client, "/api/Download/Upload?name=" + currentApp.name, path);
+        }
+
+        private void Client_UploadFileCompleted(object sender, UploadFileCompletedEventArgs e)
+        {
+            errorNewVersion = "Uploading completed.";
+            AppFileManager.Clear();
+
+            developerVM.RefreshList();
+
+            currentVersion = "Current version: " + newVersion;
+
+            Application update = new Application()
+            {
+                ID = 0,
+                name = "",
+                quickDescription = "",
+                description = "",
+                isPrivate = "",
+                mainDeveloper = 0,
+                downloadNumber = 0,
+                version = newVersion
+            };
+
+            ServerConnection.Put("/api/Applications/Update?name=" + currentApp.name, update);
+
+            newVersion = string.Empty;
+            newVersionCurrentPath = "Current path: None";
+
+            developerVM.RefreshList();
+        }
+
+        private void Client_UploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+        {
+            float progress = (float)e.BytesSent / new FileInfo("./tempApp.zip").Length;
+
+            if (progress * 100 < versionPBValue)
+                return;
+
+            versionPBValue = (float)Math.Round(progress * 100, 2);
+        }
+        #endregion
     }
 }
