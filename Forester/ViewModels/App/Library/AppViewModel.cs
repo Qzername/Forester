a3c364063148546e33d1410b4bacdfa8413a2d81
Hubert <40564309+Qzername@users.noreply.github.com>
2022-09-20 00:31:56 +0200

    bober request

--- Forester/ViewModels/App/Library/AppViewModel.cs
+++ Forester/ViewModels/App/Library/AppViewModel.cs
@@ -109,7 +109,7 @@ namespace Forester.ViewModels.App.Library
             {
                 id = currentApp.appConfig.id,
                 isDefaultPath = isDefaultPath,
-                path = path,
+                path = path +(path.EndsWith("/") ? "" : "/")+ appName + "/",
                 deleteNotNecessaryFiles = deleteNotNecessary,
             };
 
