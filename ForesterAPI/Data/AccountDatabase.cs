93bf7d4c6297bfeb620fa90e6caf64df64843325
Hubert <40564309+Qzername@users.noreply.github.com>
2023-06-28 22:38:32 +0200

    Account Database finished, Application Database started

--- ForesterAPI/Data/AccountDatabase.cs
+++ ForesterAPI/Data/AccountDatabase.cs
@@ -1,5 +1,7 @@
 ﻿using ForesterAPI.Data.Connection;
 using ForesterAPI.Models;
+using Microsoft.Win32;
+using static System.Data.Entity.Infrastructure.Design.Executor;
 
 namespace ForesterAPI.Data
 {
@@ -12,7 +14,44 @@ namespace ForesterAPI.Data
             this.sqlManager = SQLManager;
         }
 
-        public Account GetAccount(string Login) => sqlManager.SelectSingle<Account>($"SELECT * FROM Accounts WHERE Login=\"{Login}\"");
-        public Account GetAccount(int ID) => sqlManager.SelectSingle<Account>($"SELECT * FROM Accounts WHERE ID=\"{ID}\"");
+        public Account Get(string Login) => sqlManager.SelectSingle<Account>(@$"SELECT * FROM Accounts WHERE Login=""{Login}""");
+        public Account Get(int ID) => sqlManager.SelectSingle<Account>(@$"SELECT * FROM Accounts WHERE ID=""{ID}""");
+
+        /// <summary>
+        /// Creates new account
+        /// - Does not verify if data is correct
+        /// </summary>
+        public void Create(Account Account) => sqlManager.ExecuteNonQuery($"INSERT INTO Accounts(Login, Username, Password, IsDeveloper) " +
+                                                                          @$"VALUES(""{Account.Login}"", ""{Account.Username}"", ""{Account.Password}"", 0");
+        
+        public void Delete(string Login) => sqlManager.ExecuteNonQuery(@$"DELETE FROM Accounts WHERE Login=""{Login}""");
+        public void Delete(int ID) => sqlManager.ExecuteNonQuery($"DELETE FROM Accounts WHERE ID={ID}");
+
+        /// <summary>
+        /// Updates info about account
+        ///  - You can change only Username and Password
+        ///  - If neither of those are changed, query will not be executed
+        ///  - Does not verify if data is correct
+        /// </summary>
+        public void Update(Account Account)
+        {
+            string query = "UPDATE Accounts SET ";
+
+            if (!string.IsNullOrEmpty(Account.Username))
+                query += $@"Username = ""{Account.Username}"",";
+
+            if (!string.IsNullOrEmpty(Account.Password))
+                query += $@"Password = ""{Account.Password}"",";
+
+            if (query[^1] != ',')
+                return;
+
+            query = query.Remove(query.Length - 1);
+            query += @$" WHERE Login = ""{Account.Login}""";
+
+            sqlManager.ExecuteNonQuery(query);
+        }
+
+        public bool DoesExist(string Login) => Convert.ToBoolean(sqlManager.SelectSingle<int>(@$"SELECT COUNT(ID) FROM Accounts WHERE Login = ""{Login}"""));
     }
 }
---
+++ ForesterAPI/Data/ApplicationDatabase.cs
@@ -0,0 +1,15 @@
+﻿using ForesterAPI.Data.Connection;
+
+namespace ForesterAPI.Data
+{
+    public class ApplicationDatabase
+    {
+        SQLManager sqlManager;
+
+        public ApplicationDatabase(SQLManager SQLManager)
+        {
+            this.sqlManager = SQLManager;
+        }
+
+    }
+}
